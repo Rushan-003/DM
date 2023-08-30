555cd9a0758c54d348d90c94c063ca7935b2ebe9
Rushan_Hamidullin <xamidullin.rushan@gmail.com>
2023-08-30 14:55:32 +0300

    0.3

---
+++ pages/footer.php
@@ -0,0 +1,41 @@
+<footer>
+        <div class="footer"> <!-- Пересмотреть -->
+            <div>
+                <h4>Products</h4>
+                <p>Used</p>
+                <p>New</p>
+                <p>Sell your car</p>
+            </div>
+            <div>
+                <h4>Resources</h4>
+                <p>Blog</p>
+                <p>FAQ</p>
+                <p>Contact us</p>
+            </div>
+            <div>
+                <h4>Work With TrueCar</h4>
+                <p>Dealers</p>
+                <p>Partners</p>
+            </div>
+            <div>
+                <h4>About</h4>
+                <p>About us</p>
+                <p>Team</p>
+                <p>Careers</p>
+            </div>
+        </div>
+        <div class="installation">  <!-- Пересмотреть -->
+            <a href=""><img src="picture/Z_1.svg" alt=""></a>
+            <a href=""><img src="picture/Z_2.svg" alt=""></a>
+        </div>
+        <div>
+            <p id="text">For questions about the TrueCar Auto Buying Service please call 1-888-878-3227. <br>
+                Certified Dealers are contractually obligated by TrueCar to meet certain customer  service requirements and complete the TrueCar  Dealer<br> Certification Program.<br>
+                TrueCar does not broker, sell, or lease motor  vehicles. Unless otherwise noted, all vehicles shown on this website are offered for sale by<br> licensed motor vehicle dealers. All vehicles are subject to prior sale. By accessing this website, you agree to the TrueCar Terms of Service and<br> Privacy Policy.</p> <!-- Пересмотреть -->
+        </div>
+        <div class="footer_footer"> <!-- Пересмотреть -->
+            <p class="footer_element">Terms of Service</p> <!-- Пересмотреть тэг -->
+            <p class="footer_element">Privacy Policy</p> <!-- Пересмотреть тэг -->
+            <p class="footer_element">Do Not Sell My Personal Information</p> <!-- Пересмотреть тэг -->
+        </div>
+    </footer>
